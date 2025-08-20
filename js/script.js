6a2cc5aa7fd0224c0a3f52cb280e070073004b00
Cicero Junior <cicerojunior24011@gmail.com>
2025-08-19 23:31:05 -0300

    perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+//Ativar links do Menu
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink (link) {
@@ -12,3 +12,39 @@ function ativarLink (link) {
 
 
 links.forEach(ativarLink);
+
+
+// Ativar items do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto (parametro) {
+  const elemento = document.getElementById(parametro);
+  if(elemento) {
+  elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta (event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains('ativa');
+  pergunta.setAttribute('aria-expanded', ativa);
+  console.log(resposta);
+}
+
+function eventosPerguntas (pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+
