f28b482678d7cf09b4446c2ce033bfc7d10bb2f3
Cicero Junior <cicerojunior24011@gmail.com>
2025-08-21 22:45:15 -0300

    plugin animação

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
 //Ativar links do Menu
-const links = document.querySelectorAll('.header-menu a');
+const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink (link) {
+function ativarLink(link) {
   const url = location.href;
   const href = link.href;
 
-  if(url.includes(href)) {
+  if (url.includes(href)) {
     link.classList.add("ativo");
   }
 }
 
-
 links.forEach(ativarLink);
-
 
 // Ativar items do Orçamento
 const parametros = new URLSearchParams(location.search);
 
-function ativarProduto (parametro) {
+function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
-  if(elemento) {
-  elemento.checked = true;
+  if (elemento) {
+    elemento.checked = true;
   }
 }
 
@@ -28,23 +26,47 @@ parametros.forEach(ativarProduto);
 
 // Perguntas Frequentes
 
-const perguntas = document.querySelectorAll('.perguntas button');
+const perguntas = document.querySelectorAll(".perguntas button");
 
-function ativarPergunta (event) {
+function ativarPergunta(event) {
   const pergunta = event.currentTarget;
-  const controls = pergunta.getAttribute('aria-controls');
+  const controls = pergunta.getAttribute("aria-controls");
   const resposta = document.getElementById(controls);
 
-  resposta.classList.toggle('ativa');
-  const ativa = resposta.classList.contains('ativa');
-  pergunta.setAttribute('aria-expanded', ativa);
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
   console.log(resposta);
 }
 
-function eventosPerguntas (pergunta) {
-  pergunta.addEventListener('click', ativarPergunta);
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
 perguntas.forEach(eventosPerguntas);
 
+// Galeria de bicicleta
 
+const galeria = document.querySelectorAll(".caneta-imagens img");
+const galeriaContainer = document.querySelector(".caneta-imagens ");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+
+// Animação
+if(window.SimpleAnime) {
+  new SimpleAnime();
+}
